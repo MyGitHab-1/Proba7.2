ea9ac6de6a13ef2f8b15840486f13ec9be4a81b2
Vyacheslav <vip041961@yandex.ru>
2023-11-27 09:56:56 +0500

    if

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+    public class Main {
+       public static void main(String[] args) {
+
+           Scanner in = new Scanner(System.in);
+
+
+        //ОПЕРАТОР: "if"
+
+        /*int x = in.nextInt();
+        if (x == 0){
+        System.out.println("Кушал ли ты?");
+        }
+        if (x == 1){
+        System.out.println("Нет");
+        }
+        if (x<0){
+            System.out.println("Иди покушай!");
+        }*/
+
+        //ОПЕРАТОР: "ELSE"
+
+        /*int x = in.nextInt();
+        if (x == 1){
+            System.out.println("Иди работать");
+        } else {
+            System.out.println("Иди кушать");
+        }*/
+
+
+        //ОПЕРАТОР: "if"-"ELSE"-"if"
+
+        /*int x = in.nextInt();
+        if (x > 5 && x < 15) {
+            System.out.println("Это холодно");
+        } else if (x == 0) {
+            System.out.println("Это прохладно");
+        } else if (x > 0) {
+            System.out.println("Это тепло");
+        } else if (x > 20) {
+            System.out.println("Это жарко");
+        }
+
+
+       }
+
+    }
+}
